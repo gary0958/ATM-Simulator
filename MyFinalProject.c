4765a1e8d72f5adeebe67708d1abc4e75b3e9a40
Gary Johnston <gljfb0958@gmail.com>
2016-03-21 16:34:24 -0400

    Update MyFinalProject.c

--- MyFinalProject.c
+++ MyFinalProject.c
@@ -28,91 +28,84 @@ int main(int bTrue)
 	char pin[5];
 	int i=0;
 	int x=0;
-    memset(pin,0,sizeof(pin));
+        memset(pin,0,sizeof(pin));
 
 	while(i<2)
 	{
-		if(i == 0)
-		{
-		    printf("          Welome to GarBank ATM          \n\n");
-	        printf("\nPlease enter your 4 digit pin: ");
-            scanf("%s",&pin);
-		}
-		++i;
 
-		if( charLength(pin) == 4)
-		{ 
-			system("cls");
+	    if( charLength(pin) == 4)
+	    { 
+		system("cls");
 	        printf("\nWould you like a receipt?\n");
-			printf("\n    1=YES     2=NO : \n");
-			scanf("%d",&receipt);
-			break;
-        }
+		    printf("\n    1=YES     2=NO : \n");
+		    scanf("%d",&receipt);
+		    break;
+            }
 	    else
-		{
-			system("cls");
+	    {
+		system("cls");
 	        printf("\nInvalid pin, TRY AGAIN \n");
-		    printf("\nPlease enter your 4 digit pin: ");
-			memset(pin,0,sizeof(pin));
+		printf("\nPlease enter your 4 digit pin: ");
+		memset(pin,0,sizeof(pin));
 	        scanf("%s",&pin);
 
-			if( charLength(pin) == 4)
-			{
-				system("cls");
-				printf("\nWould you like a receipt?\n");
-				printf("\n   1=YES    2=NO \n");
-				scanf("%d",&receipt);
-				break;
-			}
-
-			if(i==2)
-			{
-				system("cls");
-                printf("\n\nSorry, invalid PIN \n");
-	            printf("Please contact the bank for assistance\n");
-                printf("GOODBYE\n\n");
-				bTrue = 0;
-				system("\npause");
-			    return 0;
-		     }
+		if( charLength(pin) == 4)
+		{
+			("cls");
+			printf("\nWould you like a receipt?\n");
+			printf("\n   1=YES    2=NO \n");
+			systemscanf("%d",&receipt);
+			break;
 		}
+
+		if(i==2)
+		{
+		    system("cls");
+                    printf("\n\nSorry, invalid PIN \n");
+	            printf("Please contact the bank for assistance\n");
+                    printf("GOODBYE\n\n");
+		    bTrue = 0;
+		    system("\npause");
+		    return 0;
+		 }
+	    }
 	}
 	
-    if(receipt ==1 ||receipt == 2)
+    	if(receipt ==1 ||receipt == 2)
 	{ 
-	system("cls");
-       chooseTransaction();
+	    system("cls");
+            chooseTransaction();
 	}
         while(bTrue)
-			{
-	         switch(*choice)
-		       {
-			      case 1: getCard(pin);
-					      bTrue=0;
-				          break;
+	{
+             	switch(*choice)
+	     	{
+		    case 1: getCard(pin);
+			    bTrue=0;
+		            break;
 
-                   case 2: getFastCash(pin);
-			               break;
+                    case 2: getFastCash(pin);
+			    break;
 
-		           case 3: makeWithdraw(pin);
-			               break;
+	            case 3: makeWithdraw(pin);
+		            break;
 
-		           case 4: makeDeposit(pin);
-			               break;
+		    case 4: makeDeposit(pin);
+			    break;
 
-		           case 5: getBalance(pin);
-			               break;
+	            case 5: getBalance(pin);
+			    break;
 
-		           default: system("cls");
-					        printf("Invalid choice.\n");
-					        chooseTransaction();
-              }        
-		}
+	            default: system("cls");
+			     printf("Invalid choice.\n");
+			     chooseTransaction();
+                }        
+	}
 	printf("\n");
 
 	if(bTrue==0)
 	{
-        system("pause");
+            system("pause");
 	}
 }
 int charLength(char *c)
@@ -133,14 +126,14 @@ int charLength(char *c)
 int getCard(char *pin)
 {
 	 time_t mytime;
-     mytime = time(NULL);
+         mytime = time(NULL);
 	 system("cls");
 	 if(receipt == 1)
 	 {
 		 printf("\nThank you for using GarBank\n");
 		 printf("\n          GOODBYE          \n");
 		 printf("\n\n");
-	     printf("************Receipt**********\n");
+	         printf("************Receipt**********\n");
 		 printf(" \n ");
 		 printf(ctime(&mytime));
 		 printf("\nCard Number   xxxx xxx  %c%c%c%c\n",pin[0],pin[1],pin[2],pin[3]);
@@ -150,21 +143,21 @@ int getCard(char *pin)
 		 printf("\n\n");
 		 printf("************Receipt***********\n");
 		 printf("\n\n");
-         system("pause");
-	     exit(0);
+                 system("pause");
+	         exit(0);
 	 }
 	 else
 	 {
 		 printf("\nThank you for using GarBank\n");
-	     printf("\nGOODBYE\n");
+	         printf("\nGOODBYE\n");
 		 system("pause");
-	     exit(0);
+	         exit(0);
 	 }
 }
 int getFastCash(char *pin)
 {
 	int myChoice;
-
+	
 	system("cls");
 	printf("\n    Choose amount\n");
 	printf("1 = $10          2 = $20\n");
@@ -176,7 +169,7 @@ int getFastCash(char *pin)
 	{
 		system("cls");
 		fastcash = 10;
-        printf("\nThank you.Please take your $%d\n",fastcash);
+                printf("\nThank you.Please take your $%d\n",fastcash);
 		balance-=fastcash;
 		printf("\nYour balance is $%d\n",balance);
     }
@@ -185,7 +178,7 @@ int getFastCash(char *pin)
 		system("cls");
 		fastcash = 20;
 		printf("\nThank you, Please take your $%d\n",fastcash);
-	    balance-=fastcash;
+	        balance-=fastcash;
 		printf("\nYour balance is $%d\n",balance);
 	}
 	else if(fastcash == 3)
@@ -193,7 +186,7 @@ int getFastCash(char *pin)
 		system("cls");
 		fastcash = 50;
 		printf("\nThank you, Please take your $%d\n",fastcash);
-	    balance-=fastcash;
+	        balance-=fastcash;
 		printf("\nYour balance is $%d\n",balance);
     }
 	else if(fastcash == 4)
@@ -201,11 +194,12 @@ int getFastCash(char *pin)
 		system("cls");
 		fastcash = 100;
 		printf("\nThank you, Please take your $%d\n",fastcash);
-	    balance-=fastcash;
+	        balance-=fastcash;
 		printf("\nYour balance is $%d\n",balance);
     }
 	else
-	{ system("cls");
+	{ 
+		system("cls");
 		printf("\nYou have made an invalid selection\n");
 	}
 	printf("\nWould you like another transaction?\n");
@@ -219,8 +213,7 @@ int getFastCash(char *pin)
 	}
 	if(myChoice==2)
 	{
-        getCard(pin);
-		
+                getCard(pin);
 	}
 	if(myChoice != 1 && myChoice != 2)
 	{
@@ -244,13 +237,13 @@ int makeWithdraw(char *pin)
 	else if(withdraw %10 != 0)
 	{
 		system("cls");
-        printf("\nYou must enter an amount in multiples of $10\n");
+        	printf("\nYou must enter an amount in multiples of $10\n");
 	}
 	else
 	{
 		system("cls");
-	    printf("\nThank you, please take your $%d.\n",withdraw);
-	    balance-=withdraw;
+	    	printf("\nThank you, please take your $%d.\n",withdraw);
+	    	balance-=withdraw;
 		printf("\nYour balance is $%d\n",balance);
 	}
 	printf("\nWould you like to make another transaction\n");
@@ -264,14 +257,13 @@ int makeWithdraw(char *pin)
 	}
 	if(myChoice==2)
 	{
-	    getCard(pin);
+	    	getCard(pin);
 	}
 	if(myChoice !=1 && myChoice !=2)
 	{  
 		system("cls");
 		printf("Invalid entry\n");
 		chooseTransaction();
-		
 	}
 	return 0;
 }
@@ -284,7 +276,7 @@ int makeDeposit(char *pin)
 	system("cls");
 	printf("\nThank you, your $%d deposit has been confirmed.\n",deposit);
 	printf("\nYour balance is $%d\n",balance);
-    printf("\nWould you like to make another transaction\n");
+    	printf("\nWould you like to make another transaction\n");
 	printf("\n    1=YES    2=NO\n");
 	scanf("%d",&myChoice);
 	
@@ -295,7 +287,7 @@ int makeDeposit(char *pin)
 	}
 	if(myChoice==2)
 	{
-	    getCard(pin);
+	    	getCard(pin);
 	}
 	if(myChoice!=1 && myChoice !=2)
 	{  
@@ -331,7 +323,7 @@ int getBalance(char *pin)
 }
 void chooseTransaction()
 {
-    printf("\nChoose transaction:\n");
+    	printf("\nChoose transaction:\n");
 	printf("\n1. Get card\n");
 	printf("2. Fast cash\n");
 	printf("3. Withdraw\n");
